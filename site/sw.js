e62d7638df9e988f466017db69d808b67c332f82
shadowcaramel <sharypov666@gmail.com>
2026-09-23 13:39:41 +1000

    Boat excursion rescheduled, site rebuilt

--- site/sw.js
+++ site/sw.js
@@ -4,7 +4,7 @@
    HTTPS / localhost only; the page never registers this file on plain HTTP. */
 'use strict';
 
-const CACHE = 'nucleus2026-7a3a0b78';
+const CACHE = 'nucleus2026-d5d72a7b';
 const PRECACHE = [
   './',
   './index.html',
