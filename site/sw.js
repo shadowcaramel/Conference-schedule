97a3f799da1afe13c88f550f6b6a10c1c87818e6
shadowcaramel <sharypov666@gmail.com>
2026-09-24 11:02:03 +1000

    Update programme data for NUCLEUS-2026 conference, regenerate data.js with new session details and timings; increment versioning in index.html and sw.js for cache management consistency.

--- site/sw.js
+++ site/sw.js
@@ -4,7 +4,7 @@
    HTTPS / localhost only; the page never registers this file on plain HTTP. */
 'use strict';
 
-const CACHE = 'nucleus2026-d5d72a7b';
+const CACHE = 'nucleus2026-030ccc03';
 const PRECACHE = [
   './',
   './index.html',
